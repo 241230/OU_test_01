c1e3a50e6e23a1285b64c6df4ef09948215eeaa6
MSaleniepaint <241238@edu.p.lodz.pl>
2022-01-29 18:24:39 +0100

    Nowa metoda policz()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+int count(int a, int  b)
+{
+    return a + b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W pierwszym branchu; 2+3 = " << count(2, 3) << std::endl;
     return 0;
 }
