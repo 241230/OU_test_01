a4bd5bff75eb0222391357252e8cd36ed3a59199
MSaleniepaint <241238@edu.p.lodz.pl>
2022-01-29 18:19:16 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/241230_git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "241230_git.exe"
+  "241230_git.exe.manifest"
+  "241230_git.pdb"
+  "CMakeFiles/241230_git.dir/main.cpp.obj"
+  "lib241230_git.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/241230_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
